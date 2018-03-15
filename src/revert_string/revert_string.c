f249d0509013d5d66127a22ae4b00c62cebea08c
DK <asdasdf>
2018-03-15 10:00:19 +0000

    ready

--- src/revert_string/revert_string.c
+++ src/revert_string/revert_string.c
@@ -1,7 +1,18 @@
+#include <string.h>
 #include "revert_string.h"
+
+void Swap(char *left, char *right);
 
 void RevertString(char *str)
 {
-	// your code here
+	int len = strlen(str);
+	for (int i = 0; i < len / 2; i++)
+        Swap(&str[i], &str[len - i - 1]);
 }
 
+void Swap(char *left, char *right)
+{
+	char temp = *left;
+	*left = *right;
+	*right = temp;
+}
